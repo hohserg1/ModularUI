9b7d280fbc336ae73f1fc6f2f781e56a4c1a59d6
brachy84 <brachy84@gmail.com>
2023-09-23 20:22:28 +0900

    clean up interpolation enum
    (cherry picked from commit 56e29f96717ab5f3bc163cc7fc8a397160e4b90a)

--- src/main/java/com/cleanroommc/modularui/api/drawable/IInterpolation.java
+++ src/main/java/com/cleanroommc/modularui/api/drawable/IInterpolation.java
@@ -1,9 +1,5 @@
 package com.cleanroommc.modularui.api.drawable;
 
-import cpw.mods.fml.relauncher.Side;
-import cpw.mods.fml.relauncher.SideOnly;
-import net.minecraft.client.resources.I18n;
-
 /**
  * A function which interpolates between two values.
  */
@@ -11,21 +7,5 @@ public interface IInterpolation {
 
     float interpolate(float a, float b, float x);
 
-    double interpolate(double a, double b, double x);
-
-    @SideOnly(Side.CLIENT)
-    default String getName() {
-        return I18n.format(this.getKey());
-    }
-
-    @SideOnly(Side.CLIENT)
-    String getKey();
-
-    @SideOnly(Side.CLIENT)
-    default String getTooltip() {
-        return I18n.format(this.getTooltipKey());
-    }
-
-    @SideOnly(Side.CLIENT)
-    String getTooltipKey();
+    String getName();
 }
