946f01c66e3316f31a5d1c5a46210acd48c8ea95
brachy84 <brachy84@gmail.com>
2023-10-05 04:00:12 +0900

    fix layout widgets always cover children
    (cherry picked from commit d12fc986d3214412e54cdf9e52bc9cd1f5187dc2)

--- src/main/java/com/cleanroommc/modularui/test/ResizerTest.java
+++ src/main/java/com/cleanroommc/modularui/test/ResizerTest.java
@@ -1,28 +1,37 @@
 package com.cleanroommc.modularui.test;
 
-import com.cleanroommc.modularui.api.drawable.IKey;
+import com.cleanroommc.modularui.drawable.GuiTextures;
 import com.cleanroommc.modularui.screen.ModularPanel;
 import com.cleanroommc.modularui.screen.ModularScreen;
 import com.cleanroommc.modularui.screen.viewport.GuiContext;
-import com.cleanroommc.modularui.widget.ParentWidget;
+import com.cleanroommc.modularui.utils.Alignment;
 import com.cleanroommc.modularui.widgets.ButtonWidget;
+import com.cleanroommc.modularui.widgets.layout.Column;
+import com.cleanroommc.modularui.widgets.layout.Row;
 import org.jetbrains.annotations.NotNull;
 
 public class ResizerTest extends ModularScreen {
 
     @Override
     public @NotNull ModularPanel buildUI(GuiContext context) {
+        /*TextureAtlasSprite sprite = SpriteHelper.getSpriteOfBlockState(GameObjectHelper.getBlockState("minecraft", "command_block"), EnumFacing.UP);
+        //SpriteHelper.getSpriteOfItem(new ItemStack(Items.DIAMOND));
         return ModularPanel.defaultPanel("main")
-                .coverChildrenWidth()
-                .height(40)
-                .child(new ParentWidget<>()
-                        .coverChildren()
-                        .padding(5)
-                        .child(IKey.str("A decently sized string!").asWidget().debugName("label"))
-                        .child(new ButtonWidget<>()
-                                .size(10)
-                                .top(5)
-                                .right(5)
-                                .debugName("button")));
+                .size(150)
+                .child(new DraggableWidget<>()
+                        .background(new SpriteDrawable(sprite))
+                        .size(20)
+                        .align(Alignment.Center));*/
+        return ModularPanel.defaultPanel("main")
+                .size(150)
+                .child(new Column()
+                        .alignX(0.5f)
+                        .heightRel(1f)
+                        .margin(0, 7)
+                        .coverChildrenWidth()
+                        .mainAxisAlignment(Alignment.MainAxis.SPACE_BETWEEN)
+                        .child(new ButtonWidget<>().width(40))
+                        .child(new Row().height(30).widthRel(1f).background(GuiTextures.CHECKBOARD).debugName("row"))
+                        .child(new ButtonWidget<>()));
     }
 }
