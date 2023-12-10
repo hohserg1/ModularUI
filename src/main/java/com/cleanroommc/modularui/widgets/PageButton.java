01213063b64921ca10c5adedd64b766e5f0da791
brachy84 <brachy84@gmail.com>
2023-12-10 11:22:03 +0100

    fix toggle button and page button not making sounds
    (cherry picked from commit c2458aee6f100945157eb18e8fa660bb34bb14a2)

--- src/main/java/com/cleanroommc/modularui/widgets/PageButton.java
+++ src/main/java/com/cleanroommc/modularui/widgets/PageButton.java
@@ -37,6 +37,7 @@ public class PageButton extends Widget<PageButton> implements Interactable {
     public @NotNull Result onMousePressed(int mouseButton) {
         if (!isActive()) {
             this.controller.setPage(this.index);
+            Interactable.playButtonClickSound();
             return Result.SUCCESS;
         }
         return Result.ACCEPT;
